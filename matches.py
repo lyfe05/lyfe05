7e44acd4687d3ba71fe5711fc3212720bc801694
lyfe05 <mastercarder.169@gmail.com>
2025-11-16 15:36:38 +0300

    Update matches.py

--- matches.py
+++ matches.py
@@ -206,7 +206,7 @@ def fetch_daddylive_matches():
     import pycurl
     from io import BytesIO
 
-    URL = "https://dlhd.dad/"
+    URL = "https://daddylive.sx/"
     UA = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
           "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36")
 
